690308945fbaa6838a80f6d22b61a10c7f774155
github username <cmyerskeitt@gmail.com>
2023-04-06 07:52:03 -0400

    sepereate date into seperate component

---
+++ src/components/ExpenseDate.js
@@ -0,0 +1,18 @@
+function ExpenseDate(props){
+
+    /* toISOString used to convert Date to readable format */
+    const month = props.date.toLocaleString('en-US', {month:'long'})
+    const day = props.date.toLocaleString('en-US', { day:'2-digit'})
+    const year = props.date.getFullYear()
+
+    return (
+        <div>
+            <div>{month}</div>
+            <div>{day}</div>
+            <div>{year}</div>
+    </div>
+    )
+  
+}
+
+export default ExpenseDate
