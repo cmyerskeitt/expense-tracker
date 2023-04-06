efe9a1e8c6aed1d8a1d1017cd1fdbb1921976d96
github username <cmyerskeitt@gmail.com>
2023-04-06 07:34:59 -0400

    format date and time

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,16 +2,19 @@ import './ExpenseItem.css'
 
 
 function ExpenseItem(props){
-    
-    // const expenseDate = new Date(2021,2, 28);
-    // const expenseTitle = "Car Insurance"
-    // const expenseAmount = 294.67
 
+    /* toISOString used to convert Date to readable format */
+    const month = props.date.toLocaleString('en-US', {month:'long'})
+    const day = props.date.toLocaleString('en-US', { day:'2-digit'})
+    const year = props.date.getFullYear()
 
     return (
         <div className="expense-item">
-            {/* toISOString used to convert Date to readable format */}
-            <div>{props.date.toISOString()}</div>
+            <div>
+                    <div>{month}</div>
+                    <div>{day}</div>
+                    <div>{year}</div>
+            </div>
             <div className='expense-item__description'>
                 <h2>{props.title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
